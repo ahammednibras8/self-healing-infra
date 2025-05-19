d9cf58815836cc496fa66b8987dd05f45cedaa5d
ahammednibras8 <ahammednibras@proton.me>
2025-05-19 20:02:52 +0530

    Intialize project

---
+++ src/main/java/com/ahammednibras/App.java
@@ -0,0 +1,13 @@
+package com.ahammednibras;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
